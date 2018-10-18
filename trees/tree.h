6a3d5367de59ce87de9c96498152c9c3700d6044
MinerK <s3rserafim@gmail.com>
2018-10-18 14:43:01 +0300

    tree para

--- trees/tree.h
+++ trees/tree.h
@@ -3,42 +3,43 @@ template <class K, class T>
 class tree
 {
 private:
-	struct Node
-	{
-		K key;
-		T data;
-		Node* left;
-		Node* right;
-		Node()
-		{
-			left = nullptr;
-			right = nullptr;
-		}
-		Node(K k, T d) : Node()
-		{
-			key = k;
-			data = d;
-		}
-		Node(const Node& c) : Node(c.key, c.data)
-		{
-			left = c.left;
-			right = c.right;
-		}
-	};
-	Node* root;
-	bool(*rule)(K, K);
-
+    struct Node
+    {
+        K key;
+        T data;
+        Node* left;
+        Node* right;
+        Node()
+        {
+            left = nullptr;
+            right = nullptr;
+        }
+        Node(K k, T d) : Node()
+        {
+            key = k;
+            data = d;
+        }
+        Node(const Node& c) : Node(c.key, c.data)
+        {
+            left = c.left;
+            right = c.right;
+        }
+    };
+    Node* root;
+    bool(*rule)(K, K);
+    
 public:
-	tree();
-	tree(bool(*f)(K, K));
-	tree(bool(*f)(K, K), K k, T d);
-	~tree();
-	void insert(K, T);
-	void deletenode(K);
-	bool intree(K);
-
-	bool isleaf(Node&);
-	bool isequal(K, K);
-	
+    tree();
+    tree(bool(*f)(K, K));
+    tree(bool(*f)(K, K), K k, T d);
+    ~tree();
+    
+    void insert(K, T);
+    void deletenode(K);
+    bool intree(K);
+    bool eq(K, K);
+    T search(K);
+    
+    
+    
 };
-
