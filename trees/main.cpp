1f0953259f6e0fabc2a3900ac50c8d49ff3c9383
MinerK <s3rserafim@gmail.com>
2018-10-18 14:41:30 +0300

    main.cpp para

--- trees/main.cpp
+++ trees/main.cpp
@@ -1,4 +1,4 @@
-#include "tree.h"
+#include "tree.hpp"
 #include "tree.cpp"
 
 #include <iostream>
@@ -7,10 +7,11 @@
 using namespace std;
 typedef uint_fast64_t phone;
 
-void main()
+int main()
 {
-	tree<phone, string> phonebook([](phone a, phone b) {return a < b; }, 214, "root");
-	phonebook.insert(100, "stuff");
-	cout << phonebook.intree(100);
-	system("pause");
+    tree<phone, string> phonebook([](phone a, phone b) {return a < b; }, 214, "root");
+    phonebook.insert(50, "woah");
+    cout<<phonebook.intree(214);
+    
+    return 0;
 }
